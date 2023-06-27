dc48a30e15f676e41ddbdd0d5e0487ed83f3b36a
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-26 20:39:01 -0500

    se implementa crear y actualizar cliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,23 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Cliente } from './cliente';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
-
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
-  constructor(private http: HttpClient) { }
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) {}
 
   getClientes(): Observable<Cliente[]> {
-    return this.http.get<Cliente[]>(this.urlEndPoint)
-    .pipe(
-      map((clientes:Cliente[]) => {
+    return this.http.get<Cliente[]>(this.urlEndPoint).pipe(
+      map((clientes: Cliente[]) => {
         return clientes;
       })
     );
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {
+      headers: this.httpHeaders,
+    });
+  }
+
+  getCliente(id: string): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(
+      `${this.urlEndPoint}/${cliente.id}`,
+      cliente,
+      { headers: this.httpHeaders }
+    );
+  }
+
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {
+      headers: this.httpHeaders,
+    });
   }
 }
--- src/app/clientes/form/form.component.ts
+++ src/app/clientes/form/form.component.ts
@@ -3,6 +3,7 @@ import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -12,12 +13,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormComponent {
   public cliente: Cliente = {id: 0, nombre: '', apellido: '', email: ''};
   public titulo: string = 'Crear Cliente';
+  public msjBoton: string = 'Crear';
 
   formCliente: FormGroup = this.fb.group({
     id: [''],
     nombre: ['', [Validators.required]],
     apellido: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.email]]
+    email: ['', [Validators.required, Validators.email]],
+    createAt: ['']
   });
 
   constructor(
@@ -28,11 +31,45 @@ export class FormComponent {
   ) {}
 
 
-  create(): void {
-    
+  ngOnInit(): void {
+    this.cargarCliente();
   }
 
-  update(): void {
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id: string = params['id'];
+      if(id){
+        this.clienteService.getCliente(id)
+          .subscribe((cliente: Cliente) => {
+            this.formCliente.setValue(cliente);
+            this.msjBoton = 'Editar';
+          });
+      }
+    });
+  }
 
+  create(): void {
+    this.cliente = {
+      id: this.formCliente.get('id')?.value,
+      nombre: this.formCliente.get('nombre')?.value,
+      apellido: this.formCliente.get('apellido')?.value,
+      email: this.formCliente.get('email')?.value
+    };
+
+    console.log(this.cliente);
+
+    if(this.cliente.id <= 0) {
+      this.clienteService.create(this.cliente)
+        .subscribe(cliente => {
+          this.router.navigate(['/clientes']);
+          Swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success');
+        });
+    } else {
+      this.clienteService.update(this.cliente)
+        .subscribe( cliente => {
+          this.router.navigate(['/clientes']);
+          Swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito!`, 'success');
+        });
+    }
   }
 }
